b17f55c5df3f80c245f3755f1b5c295a3a03ab9b
Ouda <oudaacs@gmail.com>
2026-04-14 22:49:58 -0500

    Added more features to the home page and a trip tracker for customer when logging in

--- CAAMSAirlineWebApp/Pages/Home.cshtml.cs
+++ CAAMSAirlineWebApp/Pages/Home.cshtml.cs
@@ -1,13 +1,30 @@
+using CAAMSAirlineWebApp.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CAAMSAirlineWebApp.Pages
 {
     [Authorize]
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public HomeModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public int TripCount { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var username = User.Identity?.Name;
+            if (username != null)
+            {
+                TripCount = await _db.Bookings
+                    .CountAsync(b => b.Customer.Username == username);
+            }
         }
     }
 }

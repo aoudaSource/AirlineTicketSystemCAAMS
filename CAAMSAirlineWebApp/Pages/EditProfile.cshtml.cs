63f86919c2a06d3e70eb71d357fa64849bc4993f
SaraMontes77 <saramontes720@gmail.com>
2026-04-16 17:58:07 -0500

    add staff feature

--- CAAMSAirlineWebApp/Pages/EditProfile.cshtml.cs
+++ CAAMSAirlineWebApp/Pages/EditProfile.cshtml.cs
@@ -44,32 +44,32 @@ namespace CAAMSAirlineWebApp.Pages
 
             if (!ModelState.IsValid) return Page();
 
-            
+
             var customerToUpdate = await _context.Customers
-                .Include(c => c.AppUser) 
+                .Include(c => c.AppUser)
                 .FirstOrDefaultAsync(c => c.CustomerId == CustomerInfo.CustomerId);
 
             if (customerToUpdate == null) return NotFound();
 
-            
+
             customerToUpdate.FirstName = CustomerInfo.FirstName;
             customerToUpdate.LastName = CustomerInfo.LastName;
             customerToUpdate.Email = CustomerInfo.Email;
             customerToUpdate.Phone = CustomerInfo.Phone;
             customerToUpdate.DOB = CustomerInfo.DOB;
 
-            
+
             if (customerToUpdate.AppUser != null)
             {
                 customerToUpdate.AppUser.FirstName = CustomerInfo.FirstName;
                 customerToUpdate.AppUser.LastName = CustomerInfo.LastName;
                 customerToUpdate.AppUser.DOB = CustomerInfo.DOB;
-                
+
             }
 
             try
             {
-                
+
                 await _context.SaveChangesAsync();
                 SuccessMessage = "Profile updated successfully!";
                 return RedirectToPage();
